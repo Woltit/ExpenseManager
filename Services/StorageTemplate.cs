90a6e608aeef308c857b04bb38f79e96d7e4cba6
Vladyslav Maievskyi <v.maievskyi@gmail.com>
2026-02-22 12:38:30 +0200

    Change desription namings for better UI output

--- Services/StorageTemplate.cs
+++ Services/StorageTemplate.cs
@@ -26,20 +26,20 @@ namespace Services
             Wallets.Add(new WalletStorageModel(privatBankCardID, "PrivatBankCard", Currency.UAH));
             Wallets.Add(new WalletStorageModel(visaCardID, "VisaCard", Currency.USD));
 
-            Transactions.Add(new TransactionStorageModel(Guid.NewGuid(), monoCardID, 50000m, ExpenseCategory.Other,"Зарплата", DateTime.Now.AddDays(-10)));
-            Transactions.Add(new TransactionStorageModel(Guid.NewGuid(), monoCardID, -500.50m, ExpenseCategory.Food, "Обід", DateTime.Now.AddDays(-9)));
-            Transactions.Add(new TransactionStorageModel(Guid.NewGuid(), monoCardID, -1500m, ExpenseCategory.Transport, "Квитки на потяг", DateTime.Now.AddDays(-9)));
-            Transactions.Add(new TransactionStorageModel(Guid.NewGuid(), monoCardID, -2000m, ExpenseCategory.Entertainment, "Парк розваг", DateTime.Now.AddDays(-7)));
-            Transactions.Add(new TransactionStorageModel(Guid.NewGuid(), monoCardID, -3000m, ExpenseCategory.Utilities, "", DateTime.Now.AddDays(-5)));
-            Transactions.Add(new TransactionStorageModel(Guid.NewGuid(), monoCardID, -2500m, ExpenseCategory.Healthcare, "Лікар", DateTime.Now.AddDays(-4)));
-            Transactions.Add(new TransactionStorageModel(Guid.NewGuid(), monoCardID, -1000m, ExpenseCategory.Education, "Книжка з програмування", DateTime.Now.AddDays(-3)));
+            Transactions.Add(new TransactionStorageModel(Guid.NewGuid(), monoCardID, 50000m, ExpenseCategory.Other,"Salary", DateTime.Now.AddDays(-10)));
+            Transactions.Add(new TransactionStorageModel(Guid.NewGuid(), monoCardID, -500.50m, ExpenseCategory.Food, "Dinner", DateTime.Now.AddDays(-9)));
+            Transactions.Add(new TransactionStorageModel(Guid.NewGuid(), monoCardID, -1500m, ExpenseCategory.Transport, "Train tickets", DateTime.Now.AddDays(-9)));
+            Transactions.Add(new TransactionStorageModel(Guid.NewGuid(), monoCardID, -2000m, ExpenseCategory.Entertainment, "Amusement park", DateTime.Now.AddDays(-7)));
+            Transactions.Add(new TransactionStorageModel(Guid.NewGuid(), monoCardID, -3000m, ExpenseCategory.Utilities, "-", DateTime.Now.AddDays(-5)));
+            Transactions.Add(new TransactionStorageModel(Guid.NewGuid(), monoCardID, -2500m, ExpenseCategory.Healthcare, "Doctor appointment", DateTime.Now.AddDays(-4)));
+            Transactions.Add(new TransactionStorageModel(Guid.NewGuid(), monoCardID, -1000m, ExpenseCategory.Education, "Programming book", DateTime.Now.AddDays(-3)));
             Transactions.Add(new TransactionStorageModel(Guid.NewGuid(), monoCardID, -4000m, ExpenseCategory.Shopping, "Zara", DateTime.Now.AddDays(-2)));
-            Transactions.Add(new TransactionStorageModel(Guid.NewGuid(), monoCardID, -3500m, ExpenseCategory.Travel, "Турагенство", DateTime.Now.AddDays(-2)));
-            Transactions.Add(new TransactionStorageModel(Guid.NewGuid(), monoCardID, -4500m, ExpenseCategory.Food, "Сільпо", DateTime.Now.AddDays(-1)));
+            Transactions.Add(new TransactionStorageModel(Guid.NewGuid(), monoCardID, -3500m, ExpenseCategory.Travel, "Travel agency", DateTime.Now.AddDays(-2)));
+            Transactions.Add(new TransactionStorageModel(Guid.NewGuid(), monoCardID, -4500m, ExpenseCategory.Food, "Silpo", DateTime.Now.AddDays(-1)));
 
 
-            Transactions.Add(new TransactionStorageModel(Guid.NewGuid(), visaCardID, 1000m, ExpenseCategory.Other, "Зарплата", DateTime.Now.AddDays(-8)));
-            Transactions.Add(new TransactionStorageModel(Guid.NewGuid(), visaCardID, -20m, ExpenseCategory.Food, "Кава з десертом", DateTime.Now.AddDays(-7)));
+            Transactions.Add(new TransactionStorageModel(Guid.NewGuid(), visaCardID, 1000m, ExpenseCategory.Other, "Salary", DateTime.Now.AddDays(-8)));
+            Transactions.Add(new TransactionStorageModel(Guid.NewGuid(), visaCardID, -20m, ExpenseCategory.Food, "Coffe and dessert", DateTime.Now.AddDays(-7)));
 
 
 
